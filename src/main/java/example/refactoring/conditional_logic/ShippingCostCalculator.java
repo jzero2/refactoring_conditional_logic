eec5c3d21c255284e24ac0be30b2d9b498249508
juyoung.kim <juyoung74.kim@linecorp.com>
2025-03-21 10:28:00 +0900

    공통로직으로 만들기. -  getRate() , getAdjustment()

--- src/main/java/example/refactoring/conditional_logic/ShippingCostCalculator.java
+++ src/main/java/example/refactoring/conditional_logic/ShippingCostCalculator.java
@@ -6,12 +6,26 @@ class ShippingCostCalculator {
     public ShippingCostCalculator(double weight) {this.weight = weight;}
 
     public double invoke() {
+        return weight * getRate() + getAdjustment();
+    }
+
+    private double getAdjustment() {
         if (weight > 20) {
-            return weight * 1.5 + 10;
+            return 10;
         } else if (weight > 10) {
-            return weight * 1.2;
+            return 0;
         } else {
-            return weight * 1.0;
+            return 0;
+        }
+    }
+
+    private double getRate() {
+        if (weight > 20) {
+            return 1.5;
+        } else if (weight > 10) {
+            return 1.2;
+        } else {
+            return 1.0;
         }
     }
 }
