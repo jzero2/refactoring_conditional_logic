dca894d36475939f2ff3b8392925e3471ea0a109
juyoung.kim <juyoung74.kim@linecorp.com>
2025-03-21 10:32:06 +0900

    Test with Coverage & 녹색 부분 남겨 두고 지우기

--- src/main/java/example/refactoring/conditional_logic/LigthWeightShippingCostCalculator.java
+++ src/main/java/example/refactoring/conditional_logic/LigthWeightShippingCostCalculator.java
@@ -5,23 +5,11 @@ public class LigthWeightShippingCostCalculator extends ShippingCostCalculator {
 
     @Override
     protected double getAdjustment() {
-        if (weight > 20) {
-            return 10;
-        } else if (weight > 10) {
-            return 0;
-        } else {
-            return 0;
-        }
+        return 0;
     }
 
     @Override
     protected double getRate() {
-        if (weight > 20) {
-            return 1.5;
-        } else if (weight > 10) {
-            return 1.2;
-        } else {
-            return 1.0;
-        }
+        return 1.0;
     }
 }
