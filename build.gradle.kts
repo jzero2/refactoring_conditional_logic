a4ffbd460d34d199182709192e9a9b9fd8ce888a
juyoung.kim <juyoung74.kim@linecorp.com>
2025-03-21 10:16:07 +0900

    remove guava dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:31.1-jre")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
