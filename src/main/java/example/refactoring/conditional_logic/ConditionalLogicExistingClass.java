f1baa77a9c6b2769f3153942b336cacadbac67ef
juyoung.kim <juyoung74.kim@linecorp.com>
2025-03-21 10:21:42 +0900

    Replace Method With Method Object - ShippingCostCalculator

--- src/main/java/example/refactoring/conditional_logic/ConditionalLogicExistingClass.java
+++ src/main/java/example/refactoring/conditional_logic/ConditionalLogicExistingClass.java
@@ -3,13 +3,22 @@ package example.refactoring.conditional_logic;
 public class ConditionalLogicExistingClass {
 
     public double calculateCost(double weight) {
-        if (weight > 20) {
-            return weight * 1.5 + 10;
-        } else if (weight > 10) {
-            return weight * 1.2;
-        } else {
-            return weight * 1.0;
-        }
+        return new ShippingCostCalculator(weight).invoke();
     }
 
+    private class ShippingCostCalculator {
+        private double weight;
+
+        public ShippingCostCalculator(double weight) {this.weight = weight;}
+
+        public double invoke() {
+            if (weight > 20) {
+                return weight * 1.5 + 10;
+            } else if (weight > 10) {
+                return weight * 1.2;
+            } else {
+                return weight * 1.0;
+            }
+        }
+    }
 }
